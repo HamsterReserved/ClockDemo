f24b5cdcbadb259b1b51dee979ae3e3db522c2b7
Hamster Tian <haotia@gmail.com>
2016-07-13 00:44:09 +0800

    Implement simple clock

--- app/src/main/java/space/hamsters/clockdemo/ClockFragment.java
+++ app/src/main/java/space/hamsters/clockdemo/ClockFragment.java
@@ -7,6 +7,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * Created by hamster on 16/7/12.
  *
@@ -14,12 +17,23 @@ import android.widget.TextView;
  */
 public class ClockFragment extends PageFragment {
     private TextView mTimeText;
+    private View mRootView;
+    private Runnable updateTimeText = new Runnable() {
+        @Override
+        public void run() {
+            Date date = new Date(System.currentTimeMillis());
+            mTimeText.setText(new SimpleDateFormat("HH:mm:ss").format(date));
+            mRootView.postDelayed(this, 500);
+        }
+    };
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_clock, container, false);
         mTimeText = (TextView)v.findViewById(R.id.text_current_time);
+        mRootView = v;
+        v.post(updateTimeText);
         return v;
     }
 
